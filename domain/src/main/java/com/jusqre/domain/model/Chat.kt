d2336a030cd684410d31bdb8a2b0ed031778945a
passiondev2024 <joycelin1124@gmail.com>
2023-02-25 17:40:47 +0900

    #feat: apply Parcelable

--- domain/src/main/java/com/jusqre/domain/model/Chat.kt
+++ domain/src/main/java/com/jusqre/domain/model/Chat.kt
@@ -1,6 +1,10 @@
 package com.jusqre.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Chat(
     var text: String,
     val isMyText: Boolean
-)
+): Parcelable
